e006836a40b805d5b08efe23e5d458df217b617b
Realche3 <Realche333@gamil.com>
2025-09-15 12:55:22 -0400

    fixed cavas error

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
     // Keep type checking enabled; set to true to allow builds with type errors
     ignoreBuildErrors: false,
   },
+  webpack: (config) => {
+    // Avoid bundling optional 'canvas' dependency pulled by pdfjs-dist in Node builds
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      canvas: false as unknown as string, // tell webpack to ignore 'canvas'
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
